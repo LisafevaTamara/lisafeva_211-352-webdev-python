fb44c0874fb4da0b23ed04ee80f2090cf7bb15f0
LisafevaTamara <chupina02@bk.ru>
2023-10-21 16:18:13 +0300

    forth_lab

---
+++ lab4/app/config.py
@@ -0,0 +1,6 @@
+SECRET_KEY = '861b1dabae5b0c120887061e55b958a4acbd2cd5850b66412f6e38cec69b0e93'
+
+MYSQL_USER = 'std_1914_lisafe'
+MYSQL_PASSWORD = 'Qwertyui'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_DATABASE = 'std_1914_lisafe'
